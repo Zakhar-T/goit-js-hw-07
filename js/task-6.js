2377bea9cd95199919e7d8623f4bc2174fd8002e
Zakhar-T <zakhar.null@gmail.com>
2025-02-16 10:20:36 +0200

    task-6 completed

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const inputEl = document.querySelector("input[type='number']");
+let amount = 0;
+
+inputEl.addEventListener("change", setAmount);
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function setAmount(event) {
+  amount =+ Number(event.currentTarget.value);
+  return amount;
+}
+
+function createBoxes() {
+  const boxes = [];
+  let size = 30 + amount * 10;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size + i * 10}px`;
+    box.style.height = `${size + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+  size += 10;
+  document.querySelector("#boxes").append(...boxes);
+}
+
+function destroyBoxes() {
+  return document.querySelector("#boxes").innerHTML = "";
+}
